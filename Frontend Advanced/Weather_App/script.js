de0bfc5d062cc37dd777bff27112a5f7e6a35ad6
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2021-07-14 18:04:30 +0300

    Weather App v1.0

--- Frontend Advanced/Weather_App/script.js
+++ Frontend Advanced/Weather_App/script.js
@@ -1,14 +1,9 @@
-function getElement(id) {
-    return document.getElementById(id);
-}
-
 var humidity,
     pressure,
     temperature,
     windSpeed,
     getWeatherButton = getElement('getWeather'),
     weatherSummary = getElement('weather-summary'),
-    loading = false,
     cityName = getElement('cityName'),
     getWeatherByCityName = getElement('getWeatherByCityName'),
     spinner,
@@ -18,9 +13,11 @@ var humidity,
     ul,
     urlCoords,
     ulCity,
-    ulCoords,
-    sunrise,
-    sunset;
+    ulCoords;
+
+function getElement(id) {
+    return document.getElementById(id);
+}
 
 function getWeather() {
     if (this.name === 'by-coords') {
@@ -29,10 +26,8 @@ function getWeather() {
         temperature = getElement(`current-temperature-${this.name}`);
         windSpeed = getElement(`current-wind-speed-${this.name}`);
         spinner = document.querySelector(`.lds-spinner.${this.name}`);
-        sunrise = getElement(`sunrise-${this.name}`);
-        sunset = getElement(`sunset-${this.name}`);
 
-        if(navigator.geolocation) {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 getWeatherData(position.coords.latitude, position.coords.longitude, null);
             })
@@ -45,8 +40,6 @@ function getWeather() {
         temperature = getElement(`current-temperature-${this.name}`);
         windSpeed = getElement(`current-wind-speed-${this.name}`);
         spinner = document.querySelector(`.lds-spinner.${this.name}`);
-        sunrise = getElement(`sunrise-${this.name}`);
-        sunset = getElement(`sunset-${this.name}`);
         currentCity = cityName.value;
 
         if (currentCity.trim() === '') {
@@ -56,15 +49,15 @@ function getWeather() {
 
         getWeatherData(null, null, currentCity);
     }
-        setTimeout(function() {
-            getWeatherByCityName.innerHTML = 'Get Weather';
-        }, 2000);
-    }
+
+    setTimeout(function () {
+        getWeatherByCityName.innerHTML = 'Get Weather';
+    }, 2000);
+}
 
 function getWeatherData(latitude, longitude, currentCity) {
     urlCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=08d1316ba8742c08076e7425c28c2614`;
     urlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=e485dc44c23d86c2cda0e9ddda2f844f`;
-    loading = true;
     ulCity = document.getElementById('by-city-name');
     ulCoords = document.getElementById('by-coords');
     ul = currentCity ? ulCity : ulCoords;
@@ -78,13 +71,10 @@ function getWeatherData(latitude, longitude, currentCity) {
         })
         .then(function (data) {
             displayData(data);
-            console.log(data);
             // setTimeout(displayData, 1000, data);
-            loading = false;
         })
         .catch(function (error) {
             console.error(error);
-            loading = false;
             spinner.classList.toggle('hidden');
             ul.classList.toggle('hidden');
         })
@@ -95,18 +85,8 @@ function displayData(data) {
     humidity.innerHTML = `${data.main.humidity} %`;
     pressure.innerHTML = `${hPaToMmHg(data.main.pressure)} mmHg`;
     windSpeed.innerHTML = `${meterPerSecToKmPerHour(data.wind.speed).toFixed(1)} km/h`;
-    sunrise.innerHTML = `${getTimes(data.sys.sunrise)}`;
-    sunset.innerHTML = `${getTimes(data.sys.sunset)}`;
-    // windSpeed.innerHTML = `${data.wind.speed} m/s`;
     spinner.classList.toggle('hidden');
     ul.classList.toggle('hidden');
-}
-
-function getTimes(sunTime) {
-    // sunTime = new Date
-    // var bb = new Date(sunTime);
-    // return bb.getHours();
-    return new Date(sunTime);
 }
 
 function hPaToMmHg(P) {
@@ -115,6 +95,7 @@ function hPaToMmHg(P) {
 
 getWeatherButton.addEventListener('click', getWeather);
 getWeatherByCityName.addEventListener('click', getWeather);
+cityName.addEventListener('change', changeButtonValue);
 
 function changeButtonValue() {
     if (currentCity.trim() === '') {
@@ -122,8 +103,6 @@ function changeButtonValue() {
     }
     getWeatherByCityName.innerHTML += ` in ${cityName.value}!`;
 }
-
-cityName.addEventListener('change', changeButtonValue);
 
 function meterPerSecToKmPerHour(metPerSec) {
     return metPerSec * 3.6;
