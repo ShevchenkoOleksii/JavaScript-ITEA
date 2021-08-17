1e7c05cb923d9034d3001e970494f4190a0dda65
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2021-08-17 13:21:25 +0300

    Weather_App_With_Speech_Recognition

---
+++ githubBattle/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+// export const App = () => {
+//     return (
+//         <h1>Hello World</h1>
+//     )
+// }
+// export const App = () => (
+//     <h1>Hello World</h1>
+// )
---
+++ githubBattle/src/index.js
@@ -0,0 +1,14 @@
+// const fn = (a, b) => a + b;
+// console.log(fn(3, 11));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {App} from "./App";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
