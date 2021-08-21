9e235a4a4c0b11d91d49650331a4a93540e449e8
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2021-08-21 16:10:32 +0300

    2048-first commit

---
+++ Frontend Advanced/2048_Game/views/matrixView.js
@@ -0,0 +1,24 @@
+function MatrixView() {
+    this.matrixModel = new MatrixModel();
+    this.template = document.getElementById('matrixTemplate').innerHTML;
+}
+
+MatrixView.prototype.render = function () {
+    var i, 
+    j, 
+    attributes = this.matrixModel.attributes,
+    str = '';
+
+    for(i = 0; i < 4; i += 1) {
+        str += `<div class='row'>`;
+
+        for(j = 0; j < 4; j += 1) {
+            str += `<div class='cell appear-${attributes.grid[i][j]}'>${attributes.grid[i][j]}</div>`;
+        }
+
+        str += `</div>`;
+    }
+
+    // console.log(str);
+    return this.template.replace(`{{matrix}}`, str);
+}
